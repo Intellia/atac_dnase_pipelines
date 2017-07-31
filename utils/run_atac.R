b1a2f722bfb251b39adc0e6aef1668c01eef88a5
murraybrad13 <murraybrad13@gmail.com>
2017-07-31 16:23:32 -0400

    run scirpt changes

--- utils/run_atac.R
+++ utils/run_atac.R
@@ -11,13 +11,12 @@ runAtac <- function(atac.bds, r1, r2, genome.build, para, samp.name){
 }
 atac.bds <- "/home/ubuntu/Documents/Github/atac_dnase_pipelines/atac.bds"  
 base.dir <- '/home/ubuntu/Documents/MiSeqRuns/170726-M03805-0207-000000000-B9PC3/'
-for(i in 1:length(ss$Sample_ID)){
-	print(i)
-	setwd(base.dir)
-	runAtac(atac.bds=atac.bds,
-		r1=paste0(base.dir, 'fastq/', ss$Sample_ID[i], '_S',i,"_L001_R1_001.fastq.gz"),
-		r2=paste0(base.dir, 'fastq/', ss$Sample_ID[i], '_S',i,"_L001_R2_001.fastq.gz"),
+ss <- c('HEKCas9', 'PHH')
+library(GuideSeq)
+registerDoMC(cores=12)
+foreach(i=1:length(ss)) %dopar% runAtac(atac.bds=atac.bds,
+		r1=paste0(base.dir, 'fastq/', ss[i], "_R1_001.fastq.gz"),
+		r2=paste0(base.dir, 'fastq/', ss[i], "_R2_001.fastq.gz"),
 		genome.build='hg38',
-		para=32,
-		samp.name=ss$Sample_ID[i])
-}
+		para=8,
+		samp.name=ss[i])
